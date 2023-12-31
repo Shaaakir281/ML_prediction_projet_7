1f977ed3e68b4fee19f4e68857fbb0e65e6d0fd8
Fathi <fathimetalsi@gmail.com>
2023-12-31 17:49:34 +0100

    build 9

--- predict/main.py
+++ predict/main.py
@@ -6,7 +6,8 @@ import pandas as pd
 # Initialisation de l'application Flask
 app = Flask(__name__)
 
-model_path = os.path.join(os.path.dirname(__file__), "model_with_threshold")
+# Utiliser la variable d'environnement pour le chemin du modèle
+model_path = os.getenv("MODEL_PATH", os.path.join(os.path.dirname(__file__), "model_with_threshold"))
 model = mlflow.pyfunc.load_model(model_path)
 
 # Route pour les prédictions
@@ -28,6 +29,7 @@ def predict():
         predictions = model.predict(data)
         return jsonify(predictions.tolist())  # Convertir les prédictions en liste pour la réponse JSON
     except Exception as e:
+        # Gérer l'exception et renvoyer un message d'erreur
         return jsonify({"error": str(e)})
 
 # Route de vérification de l'état
@@ -36,7 +38,6 @@ def health_check():
     return "API is up and running"
 
 # Démarrage de l'application
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
